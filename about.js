dc6825bef4a746849f19c8aefca31ca7c2626197
richardMattsson <mattsson.richard@gmail.com>
2024-12-31 19:00:20 +0100

    connecting the form to my email.

--- about.js
+++ about.js
@@ -14,12 +14,39 @@ if (src !== null) {
   main.insertBefore(painting, h1);
 }
 
-const submitBtn = document.querySelector("#submitBtn");
-submitBtn.addEventListener("click", (event) => {
-  console.log(event.target.value);
-});
+// const submitBtn = document.querySelector("#submitBtn");
+// submitBtn.addEventListener("click", (event) => {
+//   console.log(event.target.value);
+// });
 
 // const form = document.querySelector("#form");
 // form.addEventListener("submit", (event) => {
 //   console.log(event.target.value);
 // });
+
+(function () {
+  emailjs.init("ObogTr-cDz6R_0oug");
+})();
+
+function sendEmail(e) {
+  e.preventDefault();
+  const name = document.querySelector("#namefield").value;
+  console.log(name);
+  const message = document.querySelector("#contactMessage").value;
+  console.log(message);
+  emailjs
+    .send("service_9k925od", "template_x0pucqf", {
+      from_name: "Erica",
+      name: name,
+      message: message,
+    })
+    .then(
+      () => alert("Message sent!"),
+      (error) => {
+        console.error("Error sending email:", error);
+        alert("Failed to send message: " + error.text);
+      }
+    );
+}
+
+document.getElementById("form").addEventListener("submit", sendEmail);
